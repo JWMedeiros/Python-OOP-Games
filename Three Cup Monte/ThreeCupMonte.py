b54dda70d95eba1e82efe137f99068586ad160f3
John Medeiros <44784107+JWMedeiros@users.noreply.github.com>
2021-08-06 18:25:00 -0400

    Add files via upload

---
+++ Three Cup Monte/ThreeCupMonte.py
@@ -0,0 +1,24 @@
+#Three Cup Monte Game:
+from random import shuffle
+
+def shuffle_list(mylist):
+    shuffle (mylist)
+    return mylist
+
+def player_guess():
+    guess=''
+    while guess not in ['0','1','2']:
+        guess=input("Pick a number: 0, 1 or 2: ")
+    return int(guess)
+
+def check_guess(mylist,guess):
+    if mylist[guess]=='O':
+        print("WINNER!")
+    else:
+        print("Please Try Again.")
+        print(mylist)
+        
+mylist=[' ','O',' ']
+shuffled_list=shuffle_list(mylist)
+guess=player_guess()
+check_guess(shuffled_list,guess)
